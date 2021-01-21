32f796f1ef40d7c55aa47c4ca7bf3a5d7208253f
Eli Aloni <elix22@gmail.com>
2021-01-21 11:48:21 +0200

    using now monodis to resolve reference assemblies

--- Program.cs
+++ Program.cs
@@ -1,14 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Runtime.Loader;
+using System.Security;
 using CommandLine;
 
 namespace ReferenceAssemblyResolver
 {
 
-    public class Options
+ 
+
+    public static class ShellHelper
+    {
+
+        static string monodis_exe = "";
+
+
+        static bool FindMonoDisInDirectory(string path)
+        {
+            bool isWindows = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            bool isOSX = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+            bool isLinux = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+
+            string subFolder = "";
+            if (isWindows == true) { subFolder =  "windows"; }
+            if (isOSX == true) { subFolder = "macos"; }
+            if (isLinux == true) { subFolder = "linux"; }
+
+            string exe_name = "monodis";
+            if(isWindows) exe_name += ".exe";
+
+            string [] directories =  Directory.GetDirectories(path, subFolder, SearchOption.AllDirectories);
+
+            foreach (string directory in directories)
+            {
+                string[] fileEntries = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+                foreach (string fileName in fileEntries)
+                {
+                    if (Path.GetFileName(fileName) == exe_name)
+                    {
+                        monodis_exe = Path.GetFullPath(fileName);
+                        Console.WriteLine("monodis path : " + monodis_exe);
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
+        }
+
+        static bool FindMonoDisExe()
+        {
+            string cwd = Directory.GetCurrentDirectory();
+            //  Console.WriteLine("cwd : " + cwd);
+            string searchDirectory = cwd;
+
+            while (Directory.Exists(searchDirectory))
+            {
+                if (FindMonoDisInDirectory(searchDirectory) == true) return true;
+                searchDirectory = Path.Combine(searchDirectory, "..");
+            }
+               
+            return false;
+        }
+
+        public static string[] GetReferencedAssemblies(this string cmd)
+        {
+
+
+            if(monodis_exe == "")
+            {
+                FindMonoDisExe();
+            }
+
+            if(monodis_exe == "")
+            {
+                Console.WriteLine("Didn't find monodis");
+            }
+
+            monodis_exe = Path.GetFullPath(monodis_exe);
+
+           // Console.WriteLine("monodis path : " + monodis_exe);
+
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = monodis_exe,
+                    Arguments = "--assemblyref " + cmd,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+          
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            string [] entries = result.Split("\n");
+            List<string>  reference_assemblies = new List<string>();
+
+            foreach(string entry in entries)
+            {
+                if(entry.Contains("Name="))
+                {
+                    String name = entry.Replace("Name=", "").Replace("\r", "").Replace("\t", "");
+                    reference_assemblies.Add(name);
+                }
+            }
+
+            return reference_assemblies.ToArray();
+        }
+
+    }
+
+        public class Options
     {
         static  string[] assemblySearchPaths = new string[1] ;
        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
@@ -26,7 +139,7 @@ namespace ReferenceAssemblyResolver
     }
 
     /*
-     * ReferenceAssemblyResolver.exe   --output "E:/Development/ReferenceAssemblyResolver/temp2" --assembly "E:\Development\TestProject\bin\Release\netcoreapp3.1\Game.dll" --search "E:\Development\TestProject\bin\Release\netcoreapp3.1,E:\Development\TestProject\libs\dotnet\bcl\android,E:\Development\TestProject\libs\dotnet\bcl\android\Facades"
+     * ReferenceAssemblyResolver.exe   --output "E:/Development/ReferenceAssemblyResolver/temp2" --assembly "E:\Development\TestProject\bin\Debug\netcoreapp3.1\Game.dll" --search "E:\Development\TestProject\bin\Debug\netcoreapp3.1,E:\Development\TestProject\libs\dotnet\bcl\android,E:\Development\TestProject\libs\dotnet\bcl\android\Facades"
      */
     class Program
     {
@@ -48,6 +161,27 @@ namespace ReferenceAssemblyResolver
             foreach (var path in assemblySearchPaths)
             {
                 string assemblyFullPath = Path.Combine(path, reference.Name) + ".dll";
+                if (File.Exists(assemblyFullPath))
+                {
+                    res = true;
+                    break;
+                }
+            }
+
+            return res;
+        }
+
+        static bool referenceExist(string Name)
+        {
+            bool res = false;
+            bool hasExtention = Name.EndsWith(".dll") || Name.EndsWith(".exe");
+
+            if (hasExtention == false) Name += ".dll";
+
+
+            foreach (var path in assemblySearchPaths)
+            {
+                string assemblyFullPath = Path.Combine(path, Name);
                 if (File.Exists(assemblyFullPath))
                 {
                     res = true;
@@ -129,7 +263,7 @@ namespace ReferenceAssemblyResolver
                                 String name = assembliesList.FindLast(item => item.Equals(refe.Name));
                                 if (name == null)
                                 {
-                                    if(verbose) Console.WriteLine(refe.Name);
+                                    if(verbose) Console.WriteLine("adding reference : " + refe.Name);
                                     assembliesList.Add(refe.Name);
                                     resolveReferenceAssemblies(refe);
                                 }
@@ -144,23 +278,57 @@ namespace ReferenceAssemblyResolver
             }
         }
 
+
+        static void resolveReferenceAssemblies(String assemblyName)
+        {
+
+            bool hasExtention = assemblyName.EndsWith(".dll") || assemblyName.EndsWith(".exe");
+            if (hasExtention == false) assemblyName += ".dll";
+
+            string assemblyFullPath = getAssemblyFullPath(assemblyName);
+
+            if (assemblyFullPath != "" && File.Exists(assemblyFullPath))
+            {
+
+                File.Copy(assemblyFullPath, Path.Combine(destAssemblyPath, assemblyName), true);
+
+                if (assemblyName == "mscorlib.dll") return;
+
+                string[] reff = ShellHelper.GetReferencedAssemblies(assemblyFullPath);
+                foreach (string refe in reff)
+                {
+                    if (referenceExist(refe))
+                    {
+                        String name = assembliesList.FindLast(item => item.Equals(refe));
+                        if (name == null)
+                        {
+                            if (verbose) Console.WriteLine("adding reference : " + refe);
+                            assembliesList.Add(refe);
+                            resolveReferenceAssemblies(refe);
+                        }
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
 
-      
             Parser.Default.ParseArguments<Options>(args)
            .WithParsed(RunOptions);
+
+            string cwd = Directory.GetCurrentDirectory();
+            Console.WriteLine("cwd : "+cwd);
 
             assemblyPath = Path.GetFullPath(assemblyPath);
             Console.WriteLine("assemblyPath =  " +assemblyPath);
 
-            Assembly assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
-            AssemblyName[] reff = assembly.GetReferencedAssemblies();
+            string[] reff = ShellHelper.GetReferencedAssemblies(assemblyPath);
 
-            foreach (AssemblyName reference in reff)
+            foreach (string reference in reff)
             {
-                if (verbose)Console.WriteLine(reference.Name);
-                assembliesList.Add(reference.Name);
+                if (verbose)Console.WriteLine("adding reference : " + reference);
+                assembliesList.Add(reference);
                 resolveReferenceAssemblies(reference);
             }
         }
